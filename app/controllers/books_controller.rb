3968ba72f641d372555bb1dbc94675ef2e47eeb2
mikethreels <michaelthreels@gmail.com>
2020-11-25 22:58:07 +0100

    updated create controler so db can be update using api calls

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
-
+  skip_before_action :verify_authenticity_token 
   # GET /books
   # GET /books.json
   def index
@@ -74,6 +74,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title, :author)
+      params.require(:book).permit(:title, :category)
     end
 end
--- app/views/books/_book.json.jbuilder
+++ app/views/books/_book.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! book, :id, :title, :author, :created_at, :updated_at
+json.extract! book, :id, :title, :category, :created_at, :updated_at
 json.url book_url(book, format: :json)
